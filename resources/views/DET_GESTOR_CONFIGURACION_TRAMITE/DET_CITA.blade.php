629c3c1a96d3e6021618cd10434c5de6983debb4
Manuel Euan <meuan1990@gmail.com>
2022-08-19 12:43:23 -0500

    prueba con horario de oficinas

--- resources/views/DET_GESTOR_CONFIGURACION_TRAMITE/DET_CITA.blade.php
+++ resources/views/DET_GESTOR_CONFIGURACION_TRAMITE/DET_CITA.blade.php
@@ -1,3 +1,5 @@
+<input type="hidden" id="tramiteID" value="{id_accede_cita}">
+
 <div class="card_titulo">
     <span class="circulo" style="height: 80px;width: 80px;">
         <i class="far fa-calendar-alt"></i>
@@ -21,6 +23,15 @@
     <p style="color: #000000 !important; font-size:16px;"><strong style="font-weight:bold;">Estatus de sincronización al Portal de Citas:</strong> <label style="color: orange;" id="lblSyncCita">...Cargando...</label></p>
 </div>
 <br>
+
+<div class="parrafo">
+    <div class="col-md-12">
+        <div class="accordion" id="accordionExample">
+            <div id="htmHorario"></div>
+        </div> 
+    </div>
+</div>
+
 <div class="parrafo">
     <div class="col-md-12">
         <label style="font-size: 1rem;">
@@ -47,4 +58,95 @@
 
     validateCitasSync("lblSyncCita");
 
+    const d = new Date();
+    let text = d.toLocaleTimeString();
+
+    /***************** Manuel Euan /*****************/
+    var tramiteId =  $("#tramiteID").val();
+    /* if(getModulos){ */
+        $.ajax({
+            dataType: 'json',
+            url: "/gestores/detalleTramite/" + tramiteId + "",
+            type: "GET",
+            success: function(response) {
+                let oficinas    = response.data.oficinas;
+                let horarios    = response.data.horario;
+                let show        = 'show';
+                let html        = '';
+               
+                oficinas.forEach(oficina => {
+                    html += `
+                    <div class="card">
+                        <div class="card-header" id="heading${oficina.iId}">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse${oficina.iId}" aria-expanded="true" aria-controls="collapse${oficina.iId}">
+                                    ${oficina.Name}
+                                </button>
+                            </h2>
+                        </div>
+              
+                        <div id="collapse${oficina.iId}" class="collapse ${show}" aria-labelledby="heading${oficina.iId}" data-parent="#accordionExample">
+                            <div class="row cardHorario">
+                                <div class="col-sm-6">
+                                    <div class="card cardDias">
+                                        <div class="card-header"><label class="tituloHorario"> Horario de atención </label></div>
+                                        <div class="card-body"> `;
+                                            horarios.forEach(horario => {
+                                                if(horario.Id == oficina.Id){
+                                                    let entrada = formatAMPM(horario.OpeningHour);
+                                                    let salida  = formatAMPM(horario.ClosingHour);
+    
+                                                    html += `
+                                                        <div class="row">
+                                                            <div class="col-md-4">
+                                                                <div class="form-check form-check-inline">
+                                                                    <input class="form-check-input" type="checkbox" name="chDia${horario.diaId}"  id="chDia${horario.diaId}" value="${horario.diaId}">
+                                                                    <label class="form-check-label" for="chDia${horario.diaId}"> ${ horario.diaNombre } </label>
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-md-4"> ${ entrada } </div>
+                                                            <div class="col-md-4"> ${ salida } </div>
+                                                        </div>`;
+                                                }
+                                            });
+                                        html += `</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+                    show = '';
+                });
+
+                $("#htmHorario").html(html);
+                getModulos = false;
+            },
+            error: function(data) {
+                console.log(data)
+            }
+        });
+    /* } */
+
+    function formatAMPM(hora) {
+        let array = hora.split(':');
+        var ampm = array[0] >= 12 ? 'pm' : 'am';
+        array[0] = array[0] % 12;
+        array[0] = array[0] ? array[0] : 12; // the hour '0' should be '12'
+        array[1] = array[1] < 10 ? '0'+ array[1] : array[1];
+
+        return array[0] + ':' + array[1] + ' ' + ampm;
+    }
+
+
 </script>
+
+<style>
+    .cardHorario{
+        margin-top: 4%
+    }
+
+    .cardDias{
+        margin-left: 10%;
+        margin-bottom: 10%;
+    }
+</style>
