bddf988dc435832d9e0ab80c74092cfd680b831f
Manuel Euan <meuan1990@gmail.com>
2022-09-17 12:18:48 -0500

    correciones ortografia

--- resources/views/MSTP_LOGIN/index.blade.php
+++ resources/views/MSTP_LOGIN/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>Queretaro</title>
+    <title>Querétaro</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
      <!-- Font Awesome -->
      <link href="{{ asset('assets/template/img/favicon_queretaro/android-chrome.png') }}" rel="shortcut icon">
